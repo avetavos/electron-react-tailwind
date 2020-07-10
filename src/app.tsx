1e7475922b8babe4f1953e7e462387237081b03f
phongraphi <phongraphi@hytexts.com>
2020-07-10 14:44:12 +0700

    Fix bug

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import './index.css';
 
-const App = ():  => {
-  return (
-    <h1>Hello World</h1>
-  );
+const App: React.FC = () => {
+  return <h1>Hello World</h1>;
 };
 
 export default App;
